31f3f29d4d407cf5a581cdf1de029c6b85e45a8b
Constea <constantinarabadjiu@gmail.com>
2023-02-23 22:13:25 +0200

    Modified the code into a class

--- code.rb
+++ code.rb
@@ -1,32 +1,83 @@
+require 'json'
 
-words = File.read("google-10000-english-no-swears.txt").split
-guess_word = words[rand(1..9893)].split('')
-until guess_word.length >= 5 && guess_word.length <= 12
-    guess_word = words[rand(1..9893)].split('')
-end
-hidden_word = []
-guess_word.length.times do
-    hidden_word.push('_')
-end
-
-puts guess_word.join('')
-puts hidden_word.join('')
-
-mistakes = 0
-
-until mistakes == 6 || hidden_word.include?('_') == false
-    input_letter = gets.chomp.downcase
-    if input_letter != '' && guess_word.include?(input_letter)
-        guess_word.each_with_index do |letter, index|
-            if input_letter == letter
-                hidden_word[index] = input_letter
-                guess_word[index] = ''
-                break
-            end
+class Hangman
+    def word_forming
+        words = File.read("google-10000-english-no-swears.txt").split
+        @intact_guess_word = words[rand(1..9893)]
+        until @intact_guess_word.length >= 5 && @intact_guess_word.length <= 12
+            @intact_guess_word = words[rand(1..9893)]
         end
-    else
-        mistakes +=1
+        @guess_word = @intact_guess_word.split('')
+        @hidden_word = []
+        @guess_word.length.times do
+            @hidden_word.push('_')
+        end
+        @mistakes = 0
     end
-    print "\r" + ("\e[A\e[K"*1)
-    puts hidden_word.join('')
+    
+    def save_data
+        data = {
+            intact_guess_word: @intact_guess_word,
+            hidden_word: @hidden_word,
+            guess_word: @guess_word,
+            mistakes: @mistakes
+        }
+        File.open("data.json", "w") do |file|
+            JSON.dump(data, file)
+        end
+    end
+
+    def use_save_data
+        json_data = File.read('/mnt/c/Users/const/downloads/repos/Hangman/data.json')
+        save_data = JSON.parse(json_data)
+        @intact_guess_word = save_data['intact_guess_word']
+        @hidden_word = save_data['hidden_word']
+        @guess_word = save_data['guess_word']
+        @mistakes = save_data['mistakes']
+        
+    end
+    
+    def play
+        alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+        puts @intact_guess_word
+        puts "Introduce a letter until you complete the word, you are allowed only 6 mistakes"
+        puts "---------------------------------------------------------------------------------------------- \n\n"
+        puts "      Mistakes: #{@mistakes}\n\n"
+        puts "      #{@hidden_word.join('')}\n\n"
+        puts "----------------------------------------------------------------------------------------------"
+
+        until @mistakes == 6 || @hidden_word.include?('_') == false
+            input_letter = gets.chomp.downcase
+            until alphabet.include?(input_letter)
+                puts 'Introduce a letter!'
+                input_letter = gets.chomp.downcase
+                print "\r" + ("\e[A\e[K"*2)
+            end
+            if @guess_word.include?(input_letter)
+                @guess_word.each_with_index do |letter, index|
+                    if input_letter == letter
+                        @hidden_word[index] = input_letter
+                        @guess_word[index] = ''
+                        break
+                    end
+                end
+            else
+                @mistakes +=1
+            end
+            print "\r" + ("\e[A\e[K"*6)
+            puts "      Mistakes: #{@mistakes}\n\n"
+            puts "      #{@hidden_word.join('')}\n\n"
+            puts "----------------------------------------------------------------------------------------------"
+        end
+
+        puts ''
+        puts "The word is #{@intact_guess_word}"
+    end
 end
+
+new_game = Hangman.new
+new_game.word_forming
+new_game.save_data
+new_game.use_save_data
+new_game.play
